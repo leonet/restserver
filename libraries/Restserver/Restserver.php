4f40baaf3ade1ea993b902eda91a4386b590119b
Romain GALLIEN <romaingallien.rg@gmail.com>
2017-11-27 16:02:08 +0100

    Load classes

--- libraries/Restserver/Restserver.php
+++ libraries/Restserver/Restserver.php
@@ -1,39 +1,30 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-// Defines usefull constants
-const RESTSERVER_VERSION = '3.0.0';
-const RESTSERVER_BASEPATH = __DIR__;
+// Defines classes
+require(__DIR__.'/Core/Config');
+require(__DIR__.'/Core/Server');
+require(__DIR__.'/Core/Rule');
+require(__DIR__.'/Core/Validation');
+require(__DIR__.'/Core/Input');
+require(__DIR__.'/Core/Output');
+require(__DIR__.'/Input/Data');
+require(__DIR__.'/Input/Filter');
+require(__DIR__.'/Input/Limit');
+require(__DIR__.'/Input/Page');
+require(__DIR__.'/Input/Sorter');
+require(__DIR__.'/Input/Start');
+require(__DIR__.'/Output/Cross_domain');
+require(__DIR__.'/Output/Doc');
+require(__DIR__.'/Output/Har');
+require(__DIR__.'/Output/Response');
+require(__DIR__.'/Log/Mode');
 
 class Restserver
 {
     // Defines the CI instance
     protected $CI;
 
-    // Defines Restserver Classes to load
-    protected $_core_classes = array(
-        // Defines System Core Classes
-        'Core/Config',
-        'Core/Server',
-        'Core/Rule',
-        'Core/Validation',
-        'Core/Input',
-        'Core/Output',
-        // Defines System Input Classes
-        'Input/Data',
-        'Input/Filter',
-        'Input/Limit',
-        'Input/Page',
-        'Input/Sorter',
-        'Input/Start',
-        // Defines System Output Classes
-        'Output/Cross_domain',
-        'Output/Doc',
-        'Output/Har',
-        'Output/Response',
-        // Defines System Log Classes
-        'Log/Model'
-    );
 
     /**
      * Class cosntructor
@@ -56,20 +47,6 @@ class Restserver
      */
     public function initialize()
     {
-        if (! empty($this->_core_classes)) {
-            foreach ($this->_core_classes as $class_path) {
-                if (file_exists(RESTSERVER_BASEPATH.'/'.$class_path.'.php')) {
-                    // Require class path
-                    require_once(RESTSERVER_BASEPATH.'/'.$class_path.'.php');
-                } else {
-                    // Define class name
-                    $class_name = explode('/', $class_path);
-
-                    // Returns Class loader error
-                    show_error('Unable to locate the specified class: '.$class_name[1].'.php', 503, 'Restserver '.RESTSERVER_VERSION.' Error');
-                }
-            }
-        }
     }
 
     /**
